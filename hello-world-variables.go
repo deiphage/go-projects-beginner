ad20d157f75b7720092a791beaee0e55b1fda624
Deiphage <deiphage@gmail.com>
2016-02-29 12:16:28 -0600

    Created two new files.
    Created hello-word.go and hello-world-variables.go updated .gitignore to
    ignore .exe files
    forward: updating hello-world and hello-world-variables to include
    comments explaining the function of each line in context to the file.

---
+++ hello-world-variables.go
@@ -0,0 +1,15 @@
+// hello-world-variables
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	fmt.Println("Hello World!")
+	var age int = 25
+	var favNum float64 = 1.5496
+	randNum := 1
+
+	fmt.Println(age, favNum, randNum)
+}
---
+++ hello-world.go
@@ -0,0 +1,8 @@
+// hello-world
+package main //this is an executable file
+
+import "fmt"
+
+func main() {
+	fmt.Println("Hello World!")
+}
