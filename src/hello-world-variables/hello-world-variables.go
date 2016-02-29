fcc70a5766fb71b22c308ebd33e74c4debe1b923
Deiphage <deiphage@gmail.com>
2016-02-29 13:07:47 -0600

    quick update to the files
    Continuing changes and adding comments. removed hello world Println from
    original hello world file in hello-world-variables.go

--- src/hello-world-variables/hello-world-variables.go
+++ src/hello-world-variables/hello-world-variables.go
@@ -1,13 +1,13 @@
-package main // hello-world-variables
+// hello-world-variables
+package main // this is an executable file
 
 import (
-	"fmt"
+	"fmt" // allows us to use the Println function to print numbers
 )
 
-func main() {
-	fmt.Println("Hello World!")
-	var age int = 25
-	var favNum float64 = 1.5496
+func main() { // main function
+	var age int = 25            //creating an int with 25 as the value named age
+	var favNum float64 = 1.5496 //creating a float64 with value 1.5496 called favNum
 	randNum := 1
 
 	fmt.Println(age, favNum, randNum)
