186d638a32a7bee29abb5a3fbf366e3276be601e
Deiphage <deiphage@gmail.com>
2016-03-01 10:26:30 -0600

    Added more comments
    Just making sure I am commenting as completely as I want to.

--- src/hello-world-variables/hello-world-variables.go
+++ src/hello-world-variables/hello-world-variables.go
@@ -8,7 +8,8 @@ import (
 func main() { // main function
 	var age int = 25            //creating an int with 25 as the value named age
 	var favNum float64 = 1.5496 //creating a float64 with value 1.5496 called favNum
-	randNum := 1
+	randNum := 1                //The := syntax is shorthand for declaring and initializing a variable
+	var v1, v2 int = 1, 2       // declaring and initializing two variables at once
 
-	fmt.Println(age, favNum, randNum)
+	fmt.Println(age, favNum, randNum, v1, v2) // printing all the things
 }
